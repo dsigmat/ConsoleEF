e00cad972221092f222e2883890aaf27e0fe73f0
Denis Sidorov <dsigmat@hotmail.com>
2019-07-02 18:02:12 +0600

    gogogo

---
+++ Console_19/Migrations/20190702120041_initial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Console_19.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    GroupId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Songs_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_GroupId",
+                table: "Songs",
+                column: "GroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
--- Console_19/Program.cs
+++ Console_19/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Console_19
 {
@@ -14,10 +15,30 @@ namespace Console_19
                     Name = "Rammstain",
                     Year = 1994
                 };
+
+                var group2 = new Group()
+                {
+                    Name = "Metallica",
+                };
+
                 context.Groups.Add(group);
+                context.Groups.Add(group2);
                 context.SaveChanges();
 
-                Console.WriteLine($"id: {group.Id}, name: {group.Name}, year: {group.Year}.");
+                var songs = new List<Song>
+                {
+                    new Song(){Name = "Unforgiven", GroupId = group2.Id},
+                    new Song(){Name = "Mutter", GroupId = group.Id}
+                };
+
+                context.Songs.AddRange(songs);
+                context.SaveChanges();
+
+                foreach (var song in songs)
+                {
+                    Console.WriteLine($"Song name: {song.Name}, Song Group: {song.Group?.Name}");
+                }
+                
             }
         }
     }
